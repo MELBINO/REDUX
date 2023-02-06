06bffb5879d9ed4537304ea1112c962efb0222ec
Emilin Treesa Joseph <emilintresia02@gmail.com>
2023-02-06 21:37:14 +0530

    first commit

---
+++ react-redux/products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'The Shining by Stephen King (1977)',
+        price: 378,
+        image:require('./assets/1The Shining.png')
+
+    },
+    {
+        id: 2,
+        name: ' A Wrinkle in Time by Madeleine L’Engle (1962)',
+        price: 195,
+        image:require('./assets/2A Wrinkle.png')
+    },
+    {
+        id: 3,
+        name: 'In Cold Blood by Truman Capote',
+        price: 2400,
+        image:require('./assets/3In Cold.png')
+    },
+    {
+        id: 4,
+        name: 'Man’s Search for Meaning by Viktor E. Frankl',
+        price: 1850,
+        image:require('./assets/4man.png')
+ 
+    },
+    {
+        id: 5,
+        name: 'Dairy Of Wimby Kid',
+        price: 3040,
+        image:require('./assets/dairy.jpg')
+    },
+
+]
